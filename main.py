c4bfed54f530ba9881ab0c3af38e53e373df2569
Jheaar <arnoldochoa2001@gmail.com>
2025-10-02 01:36:48 -0500

    v2.0

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import pandas as pd
 import numpy as np
+import os
+
+ruta = r'C:\Users\Usuario\Downloads'
+
+archivos = os.listdir(ruta)
+archivos_xlsx = [archivo for archivo in archivos if archivo.endswith('.xlsx') and 'DATASET_ALUMNOS_FISI PRE_POS' in archivo]
+
+df_ = pd.read_excel(os.path.join(ruta, archivos_xlsx[0]), sheet_name='Hoja 1', engine='openpyxl')
+
+print(df_)
