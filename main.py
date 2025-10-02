abfe6622ca5bccca76c7a4c08dc7b6965c6dbda0
Jheaar <arnoldochoa2001@gmail.com>
2025-10-02 01:48:01 -0500

    v3.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 
 ruta = r'C:\Users\Usuario\Downloads'
 
@@ -8,5 +9,12 @@ archivos = os.listdir(ruta)
 archivos_xlsx = [archivo for archivo in archivos if archivo.endswith('.xlsx') and 'DATASET_ALUMNOS_FISI PRE_POS' in archivo]
 
 df_ = pd.read_excel(os.path.join(ruta, archivos_xlsx[0]), sheet_name='Hoja 1', engine='openpyxl')
-
-print(df_)
+df_ = df_[df_['ano_ingreso0'].isnull() == False]
+# quieor una figura violin de la columna nota y anon_ingreso
+plt.figure(figsize=(10, 6))
+import seaborn as sns
+sns.violinplot(x='ano_ingreso0', y='nota', data=df_)
+plt.title('Distribución de Notas por Año de Ingreso')
+plt.xlabel('Año de Ingreso')
+plt.ylabel('Nota')
+plt.show()
